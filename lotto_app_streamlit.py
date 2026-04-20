8738b391c80833bfba33ed1451b68ff1f0f9528c
SEOMW <136772489+SEOMW@users.noreply.github.com>
2026-04-20 13:08:13 +0900

    Update lotto_app_streamlit.py

--- lotto_app_streamlit.py
+++ lotto_app_streamlit.py
@@ -45,7 +45,7 @@ st.markdown("""
         align-items: center !important;
         justify-content: center !important;
         gap: 0px !important; /* 미세한 간격 */
-        width: 25% !important;
+        width: 100% !important;
         max-width: 500px !important; /* 가로 모드 시 너무 퍼지지 않게 제한 */
         margin: 0 auto !important;
         overflow: visible !important;
